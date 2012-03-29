7ebf8f5478142f62e2606a29159aa51c4485b3b6
Todd Costella <ToddCostella@gmail.com>
2012-03-29 00:38:01 -0600

    Added a larger domain example that uses a setupSpec

--- src/test/groovy/DomainExampleWithSetupSpec.groovy
+++ src/test/groovy/DomainExampleWithSetupSpec.groovy
@@ -1,4 +1,29 @@
 package test.groovy
 
-class DomainExampleWithSetupSpec {
+import main.groovy.domain.LightningTalk
+import main.groovy.domain.Person
+import spock.lang.Specification
+import spock.lang.Shared
+
+class DomainExampleWithSetupSpec extends Specification{
+    @Shared def talks = []
+    def setupSpec(){
+        Person dj = new Person(lastName: "Hagberg",firstName: "D. J.")
+        Person andrew = new Person(lastName: "Harmel-Law",firstName: "Andrew")
+        Person chris = new Person(lastName: "Phelps",firstName: "Chris")
+        talks << new LightningTalk(presenter: dj,topic: "Open Solaris")
+        talks << new LightningTalk(presenter: dj,topic: "Call before you dig")
+        talks << new LightningTalk(presenter: andrew,topic: "Pair Programming Tarot")
+        talks << new LightningTalk(presenter: chris,topic: "Japanese B&Bs")
+    }
+
+    def "All talks accounted for"(){
+        expect:
+        talks.size() == 4
+    }
+
+    def "D.J. has two presentations"(){
+        expect:
+        talks.count {it.presenter.firstName == "D. J."} == 2
+    }
 }
